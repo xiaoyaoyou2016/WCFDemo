bc8d36f5480739bbd5c2920d64b3967142eb34b3
xiaoyaoyou007 <xiaoyaoyou_007@126.com>
2018-09-16 16:46:52 +0800

    WCF服务调用验证Demo
    WCF服务调用验证示例

---
+++ WCFPro/TestWeb/testapi.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestWeb
+{
+    public partial class testapi : System.Web.UI.Page
+    {
+        protected static string ResultMsg;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResultMsg = "";
+
+            WcfTest.SalesOptClient sc = new WcfTest.SalesOptClient();
+            sc.ClientCredentials.UserName.UserName = "fox";
+            sc.ClientCredentials.UserName.Password = "123456";
+
+
+            ResultMsg = Newtonsoft.Json.JsonConvert.SerializeObject(sc.GetData());
+        }
+    }
+}
